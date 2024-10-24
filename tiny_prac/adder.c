3950aea55eaeaf4fea349d562bcbe1b6bccc29d1
joohyuk99 <joohyuk99@LAPTOP-PRFTSBD9.localdomain>
2024-10-24 20:32:47 +0900

    echo server

---
+++ tiny_prac/adder.c
@@ -0,0 +1,36 @@
+#include "csapp.h"
+
+int main() {
+    char *buf, *p;
+    char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+    int n1 = 0, n2 = 0;
+
+    // Extract two arguments
+    if((buf = getenv("QUERY_STRING") != NULL)) {
+        p = strchr(buf, '&');
+        *p = '\0';
+
+        strcpy(arg1, buf);
+        strcpy(arg2, p + 1);
+
+        n1 = atoi(arg1);
+        n2 = atoi(arg2);
+    }
+
+    // Make response body (HTML)
+    int idx = 0;
+    idx += sprintf(content + idx, "QUERY_STRING = %s&%s", buf, p + 1);
+    idx += sprintf(content + idx, "Welcome to add.com: ");
+    idx += sprintf(content + idx, "The Internet Additional Protal\r\n");
+    idx += sprintf(content + idx, "<p>The Answer is %d + %d = %d\r\n<p>", n1, n2, n1 + n2);
+    idx += sprintf(content + idx, "Thanks for visiting!\r\n");
+
+    // Generate the HTTP response
+    printf("Connection: close\r\n");
+    printf("Content-length: %d\r\n", (int)strlen(content));
+    printf("Content-type: text/html\r\n\r\n");
+    printf("%s", content);
+    fflush(stdout);
+
+    exit(0);
+}
